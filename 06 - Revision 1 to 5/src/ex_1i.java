e03e01b0671eefd178c27ef64f9e35c46c2eacd8
Nabil El Mahfoudi <0105naelmahfoudi@student.epfc.eu>
2026-06-19 21:11:50 +0200

    feat(rev1to5): exercices 1j,k and l

--- 06 - Revision 1 to 5/src/ex_1i.java
+++ 06 - Revision 1 to 5/src/ex_1i.java
@@ -1,17 +1,19 @@
 public class ex_1i {
 
 /*
-     00 01 02 03 04
-        11 12 13 14
-           22 23 24
-              33 34
-                 44
+             04
+          13 14 15
+       22 23 24 25 26
+    31 32 33 34 35 36 37
+ 40 41 42 43 44 45 46 47 48
 */
-
     public static void main(String[] args) {
         for (int row = 0; row < 5; ++row) {
-            for (int column = 0; column < 5; ++column) {
-                if (column >= row)
+            for (int column = 0; column < 9; ++column) {
+                if (column == 4
+                        || (row + column > 3 && column < 4)
+                        || (column - row <= 4 && column > 4)
+                )
                     System.out.print('X');
                 else
                     System.out.print(" ");
