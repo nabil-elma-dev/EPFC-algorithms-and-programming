9539839b25e657e60599d68abd3122d26878bcff
nabil-elma-dev <0105naelmahfoudi@student.epfc.eu>
2026-07-21 18:08:43 +0200

    feat(plateau): ex 19

--- 09 - Plateau/src/ex_19.java
+++ 09 - Plateau/src/ex_19.java
@@ -3,20 +3,19 @@ import seqint.SeqIntIterator;
 
 import java.util.Scanner;
 
-public class ex_18 {
+public class ex_19 {
     /* ANALYSIS
 
     current             2   2   3   4   4   2   2   2   6   2
     previous            /   2   3   4   4   2   2   2   6   2
 
     target == 2 in this example
-    currentPos          1   2   3   4   5   6   7   8   9   x
     currentLg           1   2   1   1   2   1   2   3   1   1
-    posTarget           1   1   1   1   1   6   6   6   6   x
-    lengthLastOfTarget  1   2   2   2   2   1   2   3   3   1
+    maxLgTarget         1   2   2   2   2   2   2   3   3   3
     */
     public static SeqIntIterator it;
     public static Scanner scan = new Scanner(System.in);
+
     public static void main() {
         System.out.println("This program prints the length and the position of the last plateau of height H (chosen by user)");
         SeqInt p = new SeqInt(2,2,3,4,4,2,2,2,6,2);
@@ -27,51 +26,43 @@ public class ex_18 {
     }
 
     public static void print (SeqInt p, int h) {
-        SeqInt res = lgLastMaxHAndPos(p,h);
-        it = res.iterator();
+        it = p.iterator();
         if (!it.hasNext()) {
             System.out.println("Unable to perform calculus, empty sequence");
         }
-        int lgLast = it.next();
-        int posLast = it.next();
 
         System.out.println(
-                (lgLast == 0 ?
+                (longestMax(p,h) == 0 ?
                         h + " has not been found"
-                        : "Last plateau of height " + h + " is on pos " + posLast + " and has length of " + lgLast)
+                        : "Longest plateau of height " + h + " has length of " + longestMax(p,h))
         );
     }
 
-    public static SeqInt lgLastMaxHAndPos(SeqInt p, int h) {
+    public static int longestMax(SeqInt p, int h) {
         it = p.iterator();
-        int lgLastH = 0;
-        int posLastH = 0;
+        int lgMax = 0;
         if (it.hasNext()) {
             int current = it.next();
             int currentLg = 1;
-            int currentPos = 1;
             if (current == h) {
-                lgLastH = currentLg;
-                posLastH = currentPos;
+                lgMax = currentLg;
             }
             while (it.hasNext()) {
                 int previous = current;
                 current = it.next();
-                ++ currentPos;
                 if (current != previous) {
                     currentLg = 1;
-                    if (current == h) {
-                        lgLastH = currentLg;
-                        posLastH = currentPos;
+                    if (current == h && lgMax < currentLg) {
+                        lgMax = currentLg;
                     }
                 } else {
                     ++ currentLg;
-                    if (current == h) {
-                        lgLastH = currentLg;
+                    if (current == h && lgMax < currentLg) {
+                        lgMax = currentLg;
                     }
                 }
             }
         }
-        return new SeqInt(lgLastH, posLastH);
+        return lgMax;
     }
 }
